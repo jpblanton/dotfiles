e0ec31b0277d507687d46780007649257a8702a0
jpblanton <jamespblanton@gmail.com>
2020-09-16 12:13:22 -0400

    vim boostrap fixes

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,15 +1,17 @@
 # install python 3.7 first
 # installing vim
+# this should check if it's on centOS - mostly useful for AWS instances so far
 sudo yum -y update
-sudo yum install ncurses-devel
-sudo yum install gcc openssl-devel bzip2-devel libffi-devel
+sudo yum -y install gcc openssl-devel bzip2-devel libffi-devel
+sudo yum -y install ncurses-devel cmake
 cd /usr/src
 sudo wget https://www.python.org/ftp/python/3.7.9/Python-3.7.9.tgz
 sudo tar xzf Python-3.7.9.tgz
 cd Python-3.7.9
-sudo ./configure --enable-optimizations
+sudo ./configure --enable-optimizations --enable-shared
 sudo make altinstall
 sudo rm /usr/src/Python-3.7.9.tgz
+echo "python 3.7 installed"
 cd ~
 git clone https://github.com/vim/vim.git
 cd vim
@@ -20,3 +22,7 @@ cd vim
 	    --enable-fail-if-missing
 sudo make
 sudo make install
+
+# look up how to loop through these again & link that way is better
+ln -sv ~/dotfiles/.vimrc ~/.vimrc
+ln -sv ~/dotfiles/.bashrc ~/.bashrc
