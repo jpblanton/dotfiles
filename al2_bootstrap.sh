0296dff943de8230183c853c47acddd7c715e617
jpblanton <jamespblanton@gmail.com>
2020-09-18 17:41:16 -0400

    debian bootstrap script

--- al2_bootstrap.sh
+++ al2_bootstrap.sh
@@ -3,7 +3,10 @@
 # this should check if it's on centOS - mostly useful for AWS instances so far
 sudo yum -y update
 sudo yum -y install gcc openssl-devel bzip2-devel libffi-devel
-sudo yum -y install ncurses-devel cmake
+sudo yum -y install ncurses-devel gcc-c++ cmake
+# needed for YouCompleteMe
+curl -sL https://rpm.nodesource.com/setup_10.x | sudo bash -
+sudo yum install -y go nodejs
 cd /usr/src
 sudo wget https://www.python.org/ftp/python/3.7.9/Python-3.7.9.tgz
 sudo tar xzf Python-3.7.9.tgz
@@ -12,6 +15,8 @@ sudo ./configure --enable-optimizations --enable-shared
 sudo make altinstall
 sudo rm /usr/src/Python-3.7.9.tgz
 echo "python 3.7 installed"
+export LDFLAGS="-rdynamic"
+export LD_LIBRARY_PATH=/lib:/usr/lib:/usr/local/lib
 cd ~
 git clone https://github.com/vim/vim.git
 cd vim
@@ -24,5 +29,7 @@ sudo make
 sudo make install
 
 # look up how to loop through these again & link that way is better
+# for file in ~/dotfiles/.{vimrc,bashrc,bash_profile,functions,aliases}; do
+# 	ln -sv 
 ln -sv ~/dotfiles/.vimrc ~/.vimrc
-ln -sv ~/dotfiles/.bashrc ~/.bashrc
+ln -sv ~/dotfiles/.bashrc ~/.bash_profile
