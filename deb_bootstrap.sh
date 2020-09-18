e6be6db7f9162fda79391cbcbfd34d08ebb6bebf
jpblanton <jamespblanton@gmail.com>
2020-09-18 18:16:50 -0400

    tweaks

--- deb_bootstrap.sh
+++ deb_bootstrap.sh
@@ -1,3 +1,11 @@
 sudo apt-get update
-sudo apt-get install vim-nox
-vim +PluginInstall
+sudo apt-get -y install vim-nox cmake python3-dev build-essential tmux
+ln -sv ~/dotfiles/.vimrc ~/.vimrc
+vim -E -c PluginInstall -c q
+cd ~/.vim/bundle/YouCompleteMe
+python3 install.py --all
+
+for file in ~/dotfiles/.[^.]*;
+	do ln -sv $file "~/${file##*/}";
+	do echo "${file##*/}";
+done
