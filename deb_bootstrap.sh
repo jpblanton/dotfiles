247ff53feff276b077a0fae46216d24eea884c03
James Blanton <jamespblanton@gmail.com>
2020-09-20 20:18:31 -0400

    tweaks to deb_bootstrap

--- deb_bootstrap.sh
+++ deb_bootstrap.sh
@@ -1,7 +1,8 @@
 sudo apt-get update
 sudo apt-get -y install vim-nox cmake python3-dev build-essential tmux
+sudo apt-get -y install npm nodejs golang-go
 ln -sv ~/dotfiles/.vimrc ~/.vimrc
-vim -E -c PluginInstall -c q
+vim -E -c PluginInstall -c q -c q
 cd ~/.vim/bundle/YouCompleteMe
 python3 install.py --all
 
